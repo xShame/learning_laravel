3c41b210b9fb735e76157d618507862ec54353b6
Kevin Eusebio <kceusebio.enclvewrx@joy-nostalg.com>
2024-03-19 01:42:22 +0800

    Your First Route and View

---
+++ resources/views/contacts.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contacts</h1>
+    <ul>
+        <li>Name: John Doe</li>
+        <li>Name: Jane Doe</li>
+        <li>Name: James Doe</li>
+        <li>Name: Joan Doe</li>
+    </ul>
+</body>
+</html>
